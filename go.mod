9e44ef51c61da97d86942a9641410627c09125d7
adetri <emosihidup@gmail.com>
2024-06-25 15:03:38 +0700

    ini init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adetri/test-pack-golang.git
+
+go 1.22.4
---
+++ say_hi.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func Sayhi2() {
+	fmt.Println("hi kamu siapa ?????")
+
+}
+func main() {
+
+}
